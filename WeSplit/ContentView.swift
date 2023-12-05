8b80ad7199e1bf3c335e0b9f83d465fc4726f83e
lysonjeada <lyscalefi@gmail.com>
2023-12-05 15:27:35 -0300

    Adiciona @FocusState na variavel amountIsFocused

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -5,6 +5,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
@@ -21,9 +22,9 @@ struct ContentView: View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Amount", value: $checkAmount,
-                              format: .currency(code: "BRL"))
-                    .keyboardType(.decimalPad)
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2 ..< 100) {
                             Text("\($0) people")
@@ -45,6 +46,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
